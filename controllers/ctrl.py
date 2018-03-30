abf75614f4abf1169d5c41834bdc94cd9066b082
stephensenjaya <senjaya.stephen@gmail.com>
2018-03-30 21:07:59 +0700

    website cari user id

--- controllers/ctrl.py
+++ controllers/ctrl.py
@@ -39,11 +39,14 @@ class Academy(http.Controller):
 	@http.route('/vouchers/detail/<int:id>/', auth='user', type='http', website=True)
 	def detail(self, id):
 
-		uid = http.request.env['membership.point.member'].get_member_by_uid
+		model_user = http.request.env['res.users']
+		id_user =	model_user._uid
+		id_member = model_user.search([['id', '=', id_user]])
 		mode = http.request.env['membership.point.voucher.setting']
 		vouc = mode.search([['is_purchaseable', '=', 'True']])
 
 		return http.request.render('ciptadlab.detail', {
+			'id_member' : id_member,
 			'id' : id,
 			'vouchers' : vouc,
 		})
@@ -51,15 +54,19 @@ class Academy(http.Controller):
 # ==========================================================================================================================
 
 	@http.route('/vouchers/purchase/', auth='user', type='http', website=True)
-	def detail(self, **kw):
+	def purchase(self, **kw):
 
-		uid = http.request.env['membership.point.member'].get_member_by_uid
-		mode = http.request.env['membership.point.voucher.setting']
-		vouc = mode.search([['id', '=', id]])
-		mode.purchaseable_member_voucher(self, vouc, kw['qty'])
+		model_member = http.request.env['membership.point.member']
+		id_member = model_member.get_member_by_uid()
+		member = model_member.serach([['id','=', id_member]])
+		model_voucher_setting = http.request.env['membership.point.voucher.setting']
+		vouc = model_voucher_setting.search([['id', '=', int(kw[voucher_id]) ]])
+		# for o in vouc :
+		# 	if()
+		mode.purchaseable_member_voucher(vouc, kw['qty'], )
 
 		return http.request.render('ciptadlab.mypurchases', {
-			'id' : id,
+			'uid' : id_member,
 			'vouchers' : vouc,
 		})
 
